4de980d1e2049beb7debb24927edc05770d25d52
GChaitanyasai <chaitu.gonuguntla15@gmail.com>
2022-09-23 17:10:41 +0530

    file added

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,14 @@
-resource "aws_vpc" "vpc_1" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.vpc_name}"
+  }
 }
 
-
+resource "aws_internet_gateway" "" {
+  vpc_id = aws_vpc..id
+  tags = {
+    Name = "${var.vpc_IGW}"
+  }
+}
